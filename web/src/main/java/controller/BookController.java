701147d7fa77c905b9a84d56fb9145ada55ab4e9
pediritti <pediritti@gmail.com>
2015-09-20 07:48:14 +0200

    Configure Book Request methods

--- web/src/main/java/controller/BookController.java
+++ web/src/main/java/controller/BookController.java
@@ -5,12 +5,14 @@ import dto.book.request.*;
 import dtos.input.BookInputDTO;
 import dtos.result.BookDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import service.BookService;
 
 import java.util.List;
 
-@RestController
+@RestController("/book")
 public class BookController {
 
     @Autowired
@@ -18,6 +20,7 @@ public class BookController {
     @Autowired
     private AddBookDTOConverter addBookDTOConverter;
 
+    @RequestMapping(method = RequestMethod.PUT, value="/register")
     public void registerBook(AddBookRequestDTO request) {
 
         BookInputDTO bookInputDTO = addBookDTOConverter.convert(request);
@@ -25,18 +28,22 @@ public class BookController {
 
     }
 
+    @RequestMapping(method = RequestMethod.PUT, value="/findIsbn")
     public void findBooks(BookIsbnRequestDTO request) {
         bookService.findBooks(request.getIsbn());
     }
 
+    @RequestMapping(method = RequestMethod.PUT, value="/findTitle")
     public void findBooksByTitle(BookTitleRequestDTO request) {
         bookService.findBooksByTitle(request.getTitle());
     }
 
+    @RequestMapping(method = RequestMethod.PUT, value="/findAuthor")
     public void findBooksByAuthor(BookAuthorRequestDTO request) {
         bookService.findBooksByAuthor(request.getAuthorId());
     }
 
+    @RequestMapping(method = RequestMethod.PUT, value="/update")
     public void updateBookTitle(BookTitleUpdateRequestDTO request) {
         List<BookDTO> books = bookService.findBooks(request.getIsbn());
         for(BookDTO book: books) {
